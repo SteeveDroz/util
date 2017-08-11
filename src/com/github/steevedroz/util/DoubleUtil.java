764db98cd4f6aa680062e87507a983ba13f01308
Steeve Droz <steeve.droz@sudest-webdesign.com>
2017-08-11 17:21:56 +0200

    Add distance and length

--- src/com/github/steevedroz/util/DoubleUtil.java
+++ src/com/github/steevedroz/util/DoubleUtil.java
@@ -14,4 +14,27 @@ public abstract class DoubleUtil {
     public static double weightedAverage(double start, double goal, double percentage) {
 	return start + (goal - start) * percentage;
     }
+
+    public static double distance(double[] start, double[] end) {
+	if (start.length != end.length) {
+	    throw new IndexOutOfBoundsException("start and end points must have the same number of coordinates");
+	}
+	double squaredDistance = 0;
+	for (int i = 0; i < start.length; i++) {
+	    squaredDistance += (end[i] - start[i]) * (end[i] - start[i]);
+	}
+	return Math.sqrt(squaredDistance);
+    }
+
+    public static double[] getDirectedLength(double[] start, double[] goal, double distance) {
+	double totalDistance = distance(start, goal);
+	double[] location = new double[start.length];
+
+	for (int i = 0; i < location.length; i++) {
+	    double ratio = (totalDistance == 0) ? 0 : distance / totalDistance;
+	    location[i] = (goal[i] - start[i]) * ratio;
+	}
+
+	return location;
+    }
 }
